4455f74d69a73532fbb2a5d80527973857bde62c
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-08 17:38:40 +0000

    removerelylatnet

--- label/model.py
+++ label/model.py
@@ -126,6 +126,48 @@ class autoencoder_333(nn.Module):   #
         x = self.decoder(z)
         return x,z
     
+
+class autoencoder_333_2(nn.Module):   # 
+    def __init__(self):            #  1x 64 x 64 
+        
+        super(autoencoder_333_2, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 64, 3, stride=1, padding=1),  # 64 * 64 * 64  
+            nn.BatchNorm2d(64), 
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),   # 64 * 31 * 31 
+            
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),  # 128 *16 * 16 
+            nn.ReLU(True),
+            nn.BatchNorm2d(128), 
+            nn.MaxPool2d(2, stride=1),  # 128 * 15 * 15 
+            
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),  # b,  * 15 * 15 
+            nn.ReLU(True),
+            nn.BatchNorm2d(64), 
+            nn.MaxPool2d(2, stride=1),  # b, 256, 14, 14 
+            
+            nn.Conv2d(64, 1, 3, stride=1, padding=1),  # b, 1  x 14, 14 
+        #    nn.BatchNorm2d(1), 
+         #   nn.ReLU(True)
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(1, 64, 3, stride=2),  # b, 2,  
+            nn.BatchNorm2d(64), 
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 128, 3, stride=2),  # b, 8, 55, 55 
+            nn.BatchNorm2d(128), 
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 3, stride=1),  # b, 16, 
+            nn.BatchNorm2d(64), 
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 1, 4, stride=1),  # b, 1,  
+        )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        x = self.decoder(z)
+        return x,z   
     
     
 class vae_501(nn.Module):  
